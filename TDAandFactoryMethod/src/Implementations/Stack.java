40af24a0c5a1e9abb48780dcd68b97cb1b595f9b
DavidUyaguariJ <david.uyaguari@udla.edu.ec>
2024-10-13 13:12:20 -0500

    add the logic of stack and queue, impleents an abstract class and custom exceptions

---
+++ TDAandFactoryMethod/src/Implementations/Stack.java
@@ -0,0 +1,42 @@
+package Implementations;
+
+import AbstractClasses.AbstractDataType;
+import Exceptions.IsEmptyException;
+
+import javax.swing.*;
+
+public class Stack extends AbstractDataType {
+    private Node top;
+
+    public Stack() {
+        top = null;
+        size = 0;
+    }
+    public void push(JTextArea textArea, int data)throws IsEmptyException{
+        Node newNode = new Node(data);
+        newNode.setNext(top);
+        top = newNode;
+        size++;
+        updateTextArea(textArea);
+    }
+    public int pop(JTextArea textArea)throws IsEmptyException{
+        isEmpty(this.top);
+        int data= top.getData();
+        top = top.getNext();
+        size--;
+        updateTextArea(textArea);
+        return data;
+    }
+
+    @Override
+    public void showData(JTextArea textArea) throws  IsEmptyException{
+        isEmpty(this.top);
+        StringBuilder stackStr= new StringBuilder();
+        Node currentNode = this.top;
+        while(currentNode!=null){
+            stackStr.append(currentNode.getData()).append("\n");
+            currentNode = currentNode.getNext();
+        }
+        textArea.setText(stackStr.toString());
+    }
+}
